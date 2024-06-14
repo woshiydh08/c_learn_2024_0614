3006b575c2579cf158a659876edc9ec079694864
yoo2hua <woshiydh08@gmail.com>
2024-06-15 01:48:54 +0800

    内存

--- Project1/13.c
+++ Project1/13.c
@@ -1,29 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-// 定义标识符常量
-#define NUM 100
+// // 定义标识符常量
+// #define NUM 100
 
-// 定义宏
-// ADD 宏名称，(参数列表)参数无类型，宏体
-#define ADD(a, b) ((a) + (b))
+// // 定义宏
+// // ADD 宏名称，(参数列表)参数无类型，宏体
+// #define ADD(a, b) ((a) + (b))
 
-int Add(int a, int b)
-{
-	return a + b;
-}
+// int Add(int a, int b)
+// {
+// 	return a + b;
+// }
 
-int main()
-{
-	// int i = NUM;
-	// int arr[NUM] = {0};
+// int main()
+// {
+// 	// int i = NUM;
+// 	// int arr[NUM] = {0};
 
-	int a = 10;
-	int b = 20;
-	int c = ADD(a, b);
+// 	int a = 10;
+// 	int b = 20;
+// 	int c = ADD(a, b);
 
-	printf("c = %d\n", c); // 添加打印语句验证结果
+// 	printf("c = %d\n", c); // 添加打印语句验证结果
 
-	return 0;
-}
+// 	return 0;
+// }
