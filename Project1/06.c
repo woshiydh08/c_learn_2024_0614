fcce642bb35c87412af5c8d24ce0a9bc130d51c4
totoro <408732839@qq.com>
2024-06-14 17:53:43 +0800

    函数

--- Project1/06.c
+++ Project1/06.c
@@ -1,31 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-// 选择语句和循环语句
-int main(void)
-{
-	int input = 0;
+// // 选择语句和循环语句
+// int main(void)
+// {
+// 	int input = 0;
 
-	scanf("%d", &input);
+// 	scanf("%d", &input);
 
-	// 选择语句
-	// if (input == 1)
-	// {
-	// 	printf("1\n");
-	// }
-	// else
-	// {
-	// 	printf("0\n");
-	// }
+// 	// 选择语句
+// 	// if (input == 1)
+// 	// {
+// 	// 	printf("1\n");
+// 	// }
+// 	// else
+// 	// {
+// 	// 	printf("0\n");
+// 	// }
 
-	// 循环语句
-	while (input != 0)
-	{
-		printf("%d\n", input);
-		scanf("%d", &input);
-	}
+// 	// 循环语句
+// 	while (input != 0)
+// 	{
+// 		printf("%d\n", input);
+// 		scanf("%d", &input);
+// 	}
 
 
-	return 0;
-}
+// 	return 0;
+// }
