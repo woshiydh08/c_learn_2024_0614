8e77f7b72368f8cc797d59d1233185c5d2975291
totoro <408732839@qq.com>
2024-06-14 18:05:01 +0800

    数组

--- Project1/07.c
+++ Project1/07.c
@@ -1,27 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
-#include <Windows.h>
+// #include <stdio.h>
+// #include <Windows.h>
 
-// 函数
+// // 函数
 
-// 求2个任意整数和
-int sum(int a, int b)
-{
-	return a + b;
-}
+// // 求2个任意整数和
+// int sum(int a, int b)
+// {
+// 	return a + b;
+// }
 
-int main(void)
-{
-	int a = 0;
-	int b = 0;
-	int c = 0;
+// int main(void)
+// {
+// 	int a = 0;
+// 	int b = 0;
+// 	int c = 0;
 
-	scanf("%d %d", &a, &b);
+// 	scanf("%d %d", &a, &b);
 
-	c = sum(a, b);
+// 	c = sum(a, b);
 
-	printf("%d\n", c);
+// 	printf("%d\n", c);
 
-	return 0;
-}
+// 	return 0;
+// }
