518797367e09da374660534171dd0f00da2d1e63
totoro <408732839@qq.com>
2024-06-14 18:42:50 +0800

    typedef关键字

--- Project1/09.c
+++ Project1/09.c
@@ -1,22 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-// 操作符
+// // 操作符
 
-int main(void)
-{
+// int main(void)
+// {
 
-	// 除号的两端都是整数的时候,执行的是整数除法,
-	// 如果两端只要有一个浮点数,那么执行的是浮点除法
+// 	// 除号的两端都是整数的时候,执行的是整数除法,
+// 	// 如果两端只要有一个浮点数,那么执行的是浮点除法
 
-	float a = 7 / 2;
+// 	float a = 7 / 2;
 
-	int b = 7 % 2; // 取模
-	// 取模操作的两个这操作符只能是整数
+// 	int b = 7 % 2; // 取模
+// 	// 取模操作的两个这操作符只能是整数
 
-	printf("a = %f\n", a);
-	printf("b = %d\n", b);
+// 	printf("a = %f\n", a);
+// 	printf("b = %d\n", b);
 
-	return 0;
-}
+// 	return 0;
+// }
