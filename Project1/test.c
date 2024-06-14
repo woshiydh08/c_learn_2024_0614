86656df16456620424b613659e37c911012e1efd
尤东华 <wolong_you@ikscrm.com>
2024-06-14 15:38:14 +0800

    添加项目文件。

---
+++ Project1/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 字符数据类型 => 1
+	printf("%zu\n", sizeof(char));
+
+	// 短整型 => 2
+	printf("%zu\n", sizeof(short));
+
+	// 整型 => 4
+	printf("%zu\n", sizeof(int));
+
+	// 长整型 => 4
+	printf("%zu\n", sizeof(long));
+
+	// 长长整型 => 8
+	printf("%zu\n", sizeof(long long));
+
+	// 单精度浮点 => 4
+	printf("%zu\n", sizeof(float));
+
+	// 双精度浮点 => 8
+	printf("%zu\n", sizeof(double));
+	
+
+	return 0;
+}
