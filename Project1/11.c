8f24e9cdc83937c12251094667e57591e225fab9
totoro <408732839@qq.com>
2024-06-14 19:00:07 +0800

    static关键字

--- Project1/11.c
+++ Project1/11.c
@@ -1,28 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-// 关键字
+// // 关键字
 
-typedef unsigned int UINT;
+// typedef unsigned int UINT;
 
-typedef struct Node
-{
-	int data;
-	struct Node *next;
-} Node;
+// typedef struct Node
+// {
+// 	int data;
+// 	struct Node *next;
+// } Node;
 
-int main(void)
-{
-	// typedef 关键字
-	// typedef 类型定义(类型重命名)
+// int main(void)
+// {
+// 	// typedef 关键字
+// 	// typedef 类型定义(类型重命名)
 
-	unsigned int num = 0;
-	UINT num2 = 0;
-	// 这里UINT就等价于unsigned
+// 	unsigned int num = 0;
+// 	UINT num2 = 0;
+// 	// 这里UINT就等价于unsigned
 
-	struct Node n;
-	Node n2;
-	// 这里Node就等价于struct Node
-	return 0;
-}
+// 	struct Node n;
+// 	Node n2;
+// 	// 这里Node就等价于struct Node
+// 	return 0;
+// }
