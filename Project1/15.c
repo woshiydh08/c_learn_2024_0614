d58a052eea78385b521be81c4dd584db4eb8cdaa
yoo2hua <woshiydh08@gmail.com>
2024-06-15 16:06:26 +0800

    判断语句

--- Project1/15.c
+++ Project1/15.c
@@ -1,33 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-/** 学生struct */
-struct Stu
-{
-	// 结构体成员
-	char name[20];
-	int age;
-	char sex[10];
-	char phone[20];
-};
+// /** 学生struct */
+// struct Stu
+// {
+// 	// 结构体成员
+// 	char name[20];
+// 	int age;
+// 	char sex[10];
+// 	char phone[20];
+// };
 
-void print(struct Stu *stu)
-{
-	// 结构体指针变量 -> 成员名
-	printf("Name: %s\n", stu->name);
+// void print(struct Stu *stu)
+// {
+// 	// 结构体指针变量 -> 成员名
+// 	printf("Name: %s\n", stu->name);
 
-	printf("Age: %d\n", (*stu).age);
-}
+// 	printf("Age: %d\n", (*stu).age);
+// }
 
-int main(void)
-{
-	struct Stu stu = {"ZhangSan", 18, "Male", "13111111111"};
+// int main(void)
+// {
+// 	struct Stu stu = {"ZhangSan", 18, "Male", "13111111111"};
 
-	// 遍历打印出学生所有信息
-	printf("Name: %s\n", stu.name);
+// 	// 遍历打印出学生所有信息
+// 	printf("Name: %s\n", stu.name);
 
-	print(&stu);
+// 	print(&stu);
 
-	return 0;
-}
+// 	return 0;
+// }
