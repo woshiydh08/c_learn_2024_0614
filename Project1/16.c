eec4c5bd95352c2d9d51216874cf4fc376ef59e5
yoo2hua <woshiydh08@gmail.com>
2024-06-15 16:12:19 +0800

    循环语句

--- Project1/16.c
+++ Project1/16.c
@@ -1,32 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	int age = 0;
-	if (scanf("%d", &age))
-	{
-		if (age >= 18)
-		{
-			printf("You can vote\n");
-		}
-		else
-		{
-			printf("You can't vote\n");
-		}
-	}
+// int main(void)
+// {
+// 	int age = 0;
+// 	if (scanf("%d", &age))
+// 	{
+// 		if (age >= 18)
+// 		{
+// 			printf("You can vote\n");
+// 		}
+// 		else
+// 		{
+// 			printf("You can't vote\n");
+// 		}
+// 	}
 
-	// switch循环
-	switch (age)
-	{
-	case 18:
-		printf("You can vote\n");
-		break;
-	default:
-		printf("You can't vote\n");
-		break;
-	}
+// 	// switch循环
+// 	switch (age)
+// 	{
+// 	case 18:
+// 		printf("You can vote\n");
+// 		break;
+// 	default:
+// 		printf("You can't vote\n");
+// 		break;
+// 	}
 
-	return 0;
-}
+// 	return 0;
+// }
---
+++ Project1/17.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	// 循环语句 for
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	printf("----------------\n");
+
+	// while语句
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+		if (i == 5)
+		{
+			break;
+		}
+	}
+
+	printf("----------------\n");
+
+	// do while语句
+	int j = 0;
+	do
+	{
+		printf("%d\n", j);
+		j++;
+	} while (j < 10);
+
+	return 0;
+}
