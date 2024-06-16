48c7b3a4ca7998bb08f92b27726e496c17bed466
yoo2hua <woshiydh08@gmail.com>
2024-06-16 16:30:32 +0800

    数组

--- Project1/20.c
+++ Project1/20.c
@@ -1,13 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#include "max_and_min.h"
+// #include "max_and_min.h"
 
-int main(void)
-{
-	int max = get_max(1, 2);
-	int min = get_min(1, 2);
-	printf("max = %d, min = %d\n", max, min);
-	return 0;
-};
+// int main(void)
+// {
+// 	int max = get_max(1, 2);
+// 	int min = get_min(1, 2);
+// 	printf("max = %d, min = %d\n", max, min);
+// 	return 0;
+// };
