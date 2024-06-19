c6d53bf8409cdf3757e8ff5bf6bef7d7540a2b61
totoro <408732839@qq.com>
2024-06-19 15:49:10 +0800

    指针减指针

--- Project1/33.c
+++ Project1/33.c
@@ -4,6 +4,8 @@
 // 指针运算
 int main(void)
 {
+
+	// 指针加减
 	int arr[10] = {0};
 	int i = 0;
 	int sz = sizeof(arr) / sizeof(arr[0]);
@@ -22,11 +24,18 @@ int main(void)
 	// }
 
 	// 写法3
-	int *p = arr;
-	for (i = 0; i < sz; i++)
-	{
-		*(p + i) = 1;
-	}
+	// int *p = arr;
+	// for (i = 0; i < sz; i++)
+	// {
+	// 	*(p + i) = 1;
+	// }
+
+	// 指针减指针
+	printf("%d\n", &arr[9] - &arr[0]);
+	// 打印9
+
+	// 指针减指针, 得到的值是指针和指针之间的元素个数
+	// 不是所有指针都能相减
 
 	return 0;
 }
